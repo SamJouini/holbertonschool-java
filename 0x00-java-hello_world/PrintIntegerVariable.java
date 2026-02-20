c96fa69bd9034d02a9727f55dc062ee0ee1398a6
SamJouini <samantha.u.h.jouini@gmail.com>
2026-02-20 14:04:33 +0100

    Java - print integer

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,6 @@
+public class PrintIntegerVariable {
+    public static void main(String[] args) {
+        int number = 1600; // You can change this value
+        System.out.println("Pennsylvania Avenue, " + number);
+    }
+}
