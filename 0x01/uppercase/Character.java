64346f6893e5d3e1e1f9fc835996eb7f131fbeb3
Sam Jouini <samsam.joujou@gmail.com>
2026-03-13 14:29:41 +0100

    Character - check if c is uppercase

---
+++ 0x01/uppercase/Character.java
@@ -0,0 +1,9 @@
+public class Character {
+    public static boolean IsUpper (char c) {
+        if (c >= 'A' && c <= 'Z') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
